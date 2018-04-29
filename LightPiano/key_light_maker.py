dad1d5c8419a0dfb35b3dabdcaa1d4aa02150a06
史丹利复合田 <tsycnh@126.com>
2018-04-29 10:08:17 +0800

    点亮工具新增亮点对齐功能。

--- LightPiano/key_light_maker.py
+++ LightPiano/key_light_maker.py
@@ -13,9 +13,11 @@ class KeyLight():
         self.piano_key_status = piano_key_status
         self.fps = fps
         self.dst = dst
-        self.add_examine_status()
+        self.add_examine_status() #增加检测状态，用来与弹琴视频对齐用。
         spot = cv2.imread('spot1.png',flags=-1)
         self.spot = cv2.resize(spot, dsize=(int(self.key_width), int(self.key_width)))
+        spot2 = cv2.imread('spot2.png',flags=-1)
+        self.spot2 = cv2.resize(spot2, dsize=(int(self.key_width), int(self.key_width)))
         ped = cv2.imread('ped.png',flags=-1)
         h, w, _ = ped.shape
         h_ = self.key_width
@@ -31,19 +33,56 @@ class KeyLight():
                 cv2.circle(self.canvas,center,radius,color=(0),thickness=-1)
             else:
                 cv2.circle(self.canvas,center,radius,color=(0,255,0),thickness=-1)
-    def draw_pic(self,status):
+    def draw_pic(self,status,split_key=False):
         self.clean_canvas()
         key_status = status[0:-1]# first 88 keys
         pedal_status = status[-1]
         for i,key in enumerate(key_status):
-
             if key > 0:
                 y1 = int((self.canvas_height- self.key_width))
                 y2 = y1 + self.key_width
                 x1 = int(i*self.key_width)
                 x2 = int((i+1)*self.key_width)
                 bg = Image.fromarray(self.canvas)
-                s = Image.fromarray(self.spot)
+                if split_key:
+                    if i%12 in [1,4,6,9,11]:
+                        s = Image.fromarray(self.spot2)
+                    else:
+                        s = Image.fromarray(self.spot)
+                else:
+                    s = Image.fromarray(self.spot)
+                bg.paste(s,box=(x1,y1),mask=s)
+                self.canvas = np.array(bg)
+            else:
+                pass
+                # cv2.circle(self.canvas,center,radius,color=(0,255,0),thickness=-1)
+
+        if pedal_status > 0:
+            # center = (int((len(key_status)-1+1/2)*self.key_width),int(self.canvas_height/2))
+            # radius = int((self.key_width * 0.8)/2)
+            # cv2.circle(self.canvas, center, radius, color=(0, 255, 0), thickness=-1)
+            bg = Image.fromarray(self.canvas)
+            s = Image.fromarray(self.ped)
+            bg.paste(s, box=(int((len(key_status)-3)*self.key_width),int(self.canvas_height*0.3)), mask=s)
+            self.canvas = np.array(bg)
+    def draw_pic2(self,status,split_key=False,position={}):
+        self.clean_canvas()
+        key_status = status[0:-1]# first 88 keys
+        pedal_status = status[-1]
+        for i,key in enumerate(key_status):
+            if key > 0:
+                y1 = int((self.canvas_height- self.key_width))
+                y2 = y1 + self.key_width
+                x1 = position[i]
+                x2 = int((i+1)*self.key_width)
+                bg = Image.fromarray(self.canvas)
+                if split_key:
+                    if i%12 in [1,4,6,9,11]:
+                        s = Image.fromarray(self.spot2)
+                    else:
+                        s = Image.fromarray(self.spot)
+                else:
+                    s = Image.fromarray(self.spot)
                 bg.paste(s,box=(x1,y1),mask=s)
                 self.canvas = np.array(bg)
             else:
@@ -63,14 +102,30 @@ class KeyLight():
         self.canvas = np.zeros(shape=(self.canvas_height,self.canvas_width,4),dtype=np.uint8)
         self.canvas += 0xEB # 背景颜色
     def add_examine_status(self):
-        examine_status = [1]*len(self.piano_key_status[0])
-        for i in range(self.fps):
+        try:
+            examine_status = [1]*len(self.piano_key_status[0])
+        except:
+            examine_status = [1] * 88
+        for i in range(2*self.fps):#插入两秒钟的验证态
             self.piano_key_status.insert(0,examine_status)
     def run(self):
         # fourcc = cv2.VideoWriter_fourcc(*'X264')# X264 quality is better than MJPG
         out = cv2.VideoWriter(self.dst, 0x00000021, self.fps, (self.canvas_width, self.canvas_height))
-        for s in self.piano_key_status:
-            self.draw_pic(s)
+        for index,s in enumerate(self.piano_key_status):
+            if index <= 2*self.fps:#前两秒单独画
+                self.draw_pic(s,split_key=True)
+            else:
+                self.draw_pic(s)
             frame = self.canvas
             out.write(frame)
         out.release()
+if __name__ == '__main__':
+    # 单元测试
+    from midi_analyzer import MidiAnalyzer
+    import midi
+    pattern = midi.read_midifile('tk.mid')
+    ma = MidiAnalyzer(pattern)
+    piano_key_status = ma.run()
+    fps = ma.fps
+    kl = KeyLight(piano_key_status,dst='./tmp.mp4',fps=fps)
+    kl.run()
--- LightPiano/main.py
+++ LightPiano/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import tkinter
+from tkinter import *
 from tkinter import filedialog
 from midi_analyzer import MidiAnalyzer
 from key_light_maker import KeyLight
@@ -7,8 +8,121 @@ import midi
 import os
 import cv2
 from shutil import copyfile
+from PIL import Image, ImageTk
+import PIL
+import numpy as np
+class FixTool(tkinter.Toplevel):
+    def __init__(self,parent):
+        super().__init__()
+        self.parent = parent
+        self.screen_height = self.parent.screenheight
+        self.all_keys = {x:14*x for x in range(88)}    #以字典形式存储的键值对，key：value ,key为亮光索引0～87，value为当前亮光的横坐标0～1279
+        self.current_light_index = 0
+        self.bind('<Key>', self.printkey)
+        self.attributes("-fullscreen", True)
+        # self.update_idletasks()
+        print(self.winfo_width(),self.winfo_height())
+        real_height = self.winfo_height()
+        self.packUI()
+        self.update_idletasks()
 
+    def resize_image(self,img,target_width):
+        w, h = img.size
+        w_new = int(target_width)
+        h_new = int((h / w) * w_new)
+        resized_img = img.resize((w_new, h_new),resample=Image.BILINEAR)
+        return resized_img
+    def load_piano_img(self):
+        origin_img = Image.open('./piano_img.png')
+        if origin_img.mode == 'RGBA':
+            png = origin_img
+            background = Image.new('RGBA', png.size, (255, 255, 255))
+            alpha_composite = Image.alpha_composite(background, png)
+            origin_img = alpha_composite
 
+        resized_img = self.resize_image(origin_img, 1280)#1280*720
+        return resized_img
+    def add_lights(self,canvas):
+        #canvas size should be 1280*720
+        kl = KeyLight([], dst='', fps=1,width=1280)
+        kl.draw_pic2(kl.piano_key_status[0],split_key=True,position=self.all_keys)
+        frame = kl.canvas
+        img = PIL.Image.fromarray(frame)
+        canvas.paste(img)
+        c = np.array(canvas)
+        return c
+    def packUI(self):
+        piano_img = self.load_piano_img().convert('RGBA')
+        self.canvas = self.add_lights(piano_img)
+        c = PIL.Image.fromarray(self.canvas).convert('RGB')
+        img = ImageTk.PhotoImage(c)
+        # img = self.canvas
+        # _,w = img._PhotoImage__size
+        # w = int(img._PhotoImage__size[0])
+        # frm_0 = tkinter.Frame(self,width=w)
+        # frm_0.pack(side='left')
+
+        frm_L = tkinter.Frame(self)
+        self.label = Label(frm_L, image=img)
+        self.label.photo = img
+        self.label.pack()
+        frm_L.pack(side='left')
+
+        frm_R = tkinter.Frame(self)
+        self.label2 = Label(frm_R)
+        self.label2['text'] = '请设置'
+        self.label2.pack()
+        frm_R.pack(side='left')
+    def update_index(self,plus=1):
+        #更新亮光索引
+        self.current_light_index += plus
+        if self.current_light_index > 88-1:
+            self.current_light_index = 0
+        if self.current_light_index < 0:
+            self.current_light_index = 88-1
+    def update_key_position(self,key_index,plus=1):
+        self.all_keys[key_index]+=plus
+    def printkey(self,event):
+        print('你按下了: ' + event.char,'keycode:',event.keycode)# <KeyPress event state=Mod3 keysym=KP_Enter keycode=4980739 char='\x03' delta=4980739 x=-776 y=-53>
+        print(event)
+        next_light_keys=['Down']
+        previous_light_keys=['Up']
+        move_right_keys = ['Right']
+        move_left_keys = ['Left']
+        exit_keys = ['Escape']
+        if(event.keysym in next_light_keys):
+            self.update_index(plus=1)
+            print('当前琴键：', self.current_light_index)
+        elif(event.keysym in previous_light_keys):
+            self.update_index(plus=-1)
+            print('当前琴键：', self.current_light_index)
+        elif(event.keysym in move_right_keys):
+            self.update_key_position(key_index=self.current_light_index,plus=1)
+            print('亮光右移')
+        elif(event.keysym in move_left_keys):
+            self.update_key_position(key_index=self.current_light_index,plus=-1)
+            print('亮光左移')
+
+        elif(event.keysym in exit_keys):
+            self.destroy()
+        # img = ImageTk.PhotoImage(self.all_scores[self.current_light_index])
+        # self.label.configure(image=img)
+        # self.label.photo = img
+        #
+        # img2 = ImageTk.PhotoImage(self.all_scores[self.current_light_index + 1])
+        # self.label2.configure(image=img2)
+        # self.label2.photo = img2
+    # def setup(self):
+    #     self.center_window(self.root, 600, 300)
+    #     self.root.minsize(300, 300)
+    #     self.root.maxsize(600, 600)
+    #     self.root.title('亮光位置修复工具')
+    # def center_window(self,root, width, height):
+    #     screenwidth = root.winfo_screenwidth()
+    #     screenheight = root.winfo_screenheight()
+    #     size = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+    #     print(size)
+    #     root.geometry(size)
 class App(tkinter.Frame):
     def __init__(self,master=None):
         super().__init__(master)
@@ -18,14 +132,12 @@ class App(tkinter.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.btn0 = tkinter.Button(self.root, text='选择midi文件', command=self.open_file)
-        self.label1 = tkinter.Label(self.root)
-        self.btn1 = tkinter.Button(self.root, text='开始导出按键亮光视频',command=self.do_actions)
-        self.label2 = tkinter.Label(self.root)
-        self.btn0.pack()
-        self.label1.pack()
-        self.btn1.pack()
-        self.label2.pack()
+        self.btn1 = tkinter.Button(self.root, text='选择midi文件', command=self.open_file).pack()
+        self.label1 = tkinter.Label(self.root).pack()
+        self.btn2 = tkinter.Button(self.root, text='开始导出按键亮光视频', command=self.do_actions).pack()
+        self.label2 = tkinter.Label(self.root).pack()
+        self.btn3 = tkinter.Button(self.root,text='修正亮光位置',command=self.show_fixtool).pack()
+
 
     def get_screen_size(self,window):
         return window.winfo_screenwidth(), window.winfo_screenheight()
@@ -36,6 +148,7 @@ class App(tkinter.Frame):
     def center_window(self,root, width, height):
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
+        self.screenheight = screenheight
         size = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         print(size)
         root.geometry(size)
@@ -74,7 +187,9 @@ class App(tkinter.Frame):
         if os.path.isfile(output_tmp_file_name):
             os.remove(output_tmp_file_name)
         self.label2['text'] = '视频导出完成,文件存储为：\n'+output_file_name
-
+    def show_fixtool(self):
+        fix_tool = FixTool(self)
+        self.wait_window(fix_tool)
     def do_actions(self):
         self.analysie_midi()
         self.export_video()
